ca64ce6b5642c1a5cf3722c1d079268dee3812ef
Louioskarmen <ayxc000@qq.com>
2025-04-28 21:13:59 +0800

    4.28.2

--- React练习/r-01/src/App.jsx
+++ React练习/r-01/src/App.jsx
@@ -1,8 +1,23 @@
-
+import './'
+import {useState} from 'react'
 function App() {
+
+  const [a,seta]=useState(0)
+  const [b,setb]=useState({name:'zhangsan'})
+  const [c,setc]=useState({name:'xgm'})
+
+  const k=()=>{
+    setc({...c,name:'wangwu'})
+  }
+
   return (
     <div className="App"> 
-        <h1>Hello React!</h1>
+ 
+        <button onClick={()=>seta(a+1)}>{a}</button>
+
+        <button onClick={()=>setb({...b,name:'lisi'})}>{b.name}</button>
+
+        <button onClick={k}>{c.name}</button>
     </div>
   )
 
