98afc9a07d1f0560e266415f9df15a0f097104b6
Louioskarmen <ayxc000@qq.com>
2025-05-08 22:27:02 +0800

    hhh

---
+++ React练习/r-00/src/pages/my.tsx
@@ -0,0 +1,8 @@
+function My() {
+  return (
+    <div>
+      <h1>My</h1>
+    </div>
+  );
+}
+export default My;
--- React练习/r-01/src/App.jsx
+++ React练习/r-01/src/App.jsx
@@ -1,19 +1,39 @@
 import './index.css'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
 
-function Hhh(){
+function Hhh(props){
   
   const [a,seta]=useState(0)
   const [b,setb]=useState({name:'zhangsan'})
+  
   const [c,setc]=useState({name:'xgm'})
+
+  //useState的参数是一个函数，返回值是一个对象
+  //也可以叫惰性初始化
+  const [n,setn]=useState(()=>
+  {
+    return props.num
+  })
+
+  const [count,setcount]=useState(0)
 
   const k=()=>{
     setc({...c,name:'wangwu'})
   }
+
+  //副作用：只要不是props/state的变化，都会触发
+  useEffect(()=>{
+    document.title=`你点击了${count}次`
+  })
+
   return (
     <div> 
+
+        <button onClick={()=>setn(n+1)}>{n}</button>
  
+        <button onClick={()=>setcount(count+1)}>{count}</button>
+
         <button onClick={()=>seta(a+1)}>{a}</button>
 
         <button onClick={()=>setb({...b,name:'lisi'})}>{b.name}</button>
@@ -39,7 +59,7 @@ function App() {
 
   return (
     <div className="App"> 
-      <Hhh/>
+      <Hhh num={100}/>
       <Vmodel/>
     </div>
   )
